8dfa6695ea26629d80a0b58b27201a84798e6c56
Younes <bdryounesgh@gmail.com>
2023-08-10 03:59:42 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	printf("and that piece of art is useful\" - Dora Korp
+			ar, 2015-10-19\n", stdout);
 return (1);
 }
