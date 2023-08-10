6728433b8c1bccbc42c48775815403fe04451a92
Younes <bdryounesgh@gmail.com>
2023-08-10 03:15:45 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)", sizeof(char));
+	printf("Size of an int: %d bytes(s)", sizeof(int));
+	printf("Size of a long int:%d bytes(s)", sizeof(long int));
+	printf("Size of a long long: %d bytes(s)", sizeof(long long int));
+	printf("Size of a float: %d bytes(s)", sizeof(float));
+return (0);
+}
