72e67f51e5a8b2f9d1a66443c7625df7d7fe27f4
Younes <bdryounesgh@gmail.com>
2023-08-11 16:27:09 -0700

    numbers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+	{
+	int ch;
+
+	for (ch = 122; ch > 96; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+return (0);
+
+}
