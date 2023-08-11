7bd92ac62a53c7aa83abaf71a7d0c6f2f5a13ff7
Younes <bdryounesgh@gmail.com>
2023-08-11 16:50:46 -0700

    numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+	{
+	int c;
+
+	for (c = 48 ; c < 58 ; c++)
+	{
+	putchar(c);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+return (0);
+
+}
