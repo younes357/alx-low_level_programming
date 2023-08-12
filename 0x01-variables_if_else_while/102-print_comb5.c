8b5f5d18db826913ce774869f569c4b1400655df
Younes <bdryounesgh@gmail.com>
2023-08-11 19:21:53 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,14 +16,14 @@ for (i = 0; i <= 99; i++)
 	{
 	for (j = i + 1; j <= 99; j++)
 	{
-		if (i<j)
+		if (i < j)
 		{
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
 		putchar(' ');
 		putchar((j / 10) + '0');
 		putchar((j % 10) + '0');
-		if (i == 98 && j == 99))
+		if (i == 98 && j == 99)
 		{
 		putchar(',');
 		putchar(' ');
