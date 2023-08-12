fc1bad6072688f844712af976552facc2def8321
Younes <bdryounesgh@gmail.com>
2023-08-11 18:39:49 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+	{
+	int i, j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+		if (i < j || i != j)
+		{
+		putchar(i);
+		putchar(j);
+		}
+		if (i == 56 && j == 57)
+		{
+		putchar('\n');
+		}
+		else
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+
+	}
+return (0);
+
+}
