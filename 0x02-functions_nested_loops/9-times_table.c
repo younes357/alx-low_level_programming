1d47fb69bba71eb1ada6d2e3129aac9c7f09c202
Younes <bdryounesgh@gmail.com>
2023-08-15 16:30:26 -0700

    24 hours

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,31 @@
  */
 void times_table(void)
 {
-	int i, j, k = 0;
-
+		int i, j;
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 		k = i * j;
+		if (k < 10)
+		{
 		_putchar (k + '0');
 		_putchar (',');
+		_putchar (' ');
+		}
+		else if (k >= 10)
+		{
+		_putchar ((k / 10) + '0');
+		_putchar ((k % 10) + '0');
+		_putchar (',');
+		_putchar (' ');
 		}
 		_putchar ('\n');
 	}
+
 }
+
+
+
+
+
