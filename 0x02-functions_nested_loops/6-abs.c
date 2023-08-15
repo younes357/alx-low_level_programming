323657730e4c5769d1d5d2eb043f8101e8a782de
Younes <bdryounesgh@gmail.com>
2023-08-15 04:55:12 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _abs - determine abs of n
+ * @n: The character to check
+ *
+ * Return: 1 if lowercase or 0 if not.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
