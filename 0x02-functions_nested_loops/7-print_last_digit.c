272fc813fc70db7831f8618cc017d599e143116d
Younes <bdryounesgh@gmail.com>
2023-08-15 05:00:46 -0700

    abs

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - determine last digit of number
+ * @n: The number to check
+ *
+ * Return: 1 if lowercase or 0 if not.
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
+
