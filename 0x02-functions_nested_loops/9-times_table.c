7e4c6e30958f9f4b41017ec35af6c07e8c2d22fd
Younes <bdryounesgh@gmail.com>
2023-08-15 16:55:13 -0700

    24 hours

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,41 +7,41 @@
  */
 void times_table(void)
 {
-                int i, j, p = 0;
-        for (i = 0; i <= 9; i++)
-        {
-                for (j = 0; j <= 9; j++)
-                {
-                p = i * j;
-                if (p < 10 && j != 9)
-                {
-                _putchar (p + '0');
-                _putchar (',');
-                _putchar (' ');
-                }
-                else if (p >= 10 && j != 9)
-                {
-                _putchar ((p / 10) + '0');
-                _putchar ((p % 10) + '0');
-                _putchar (',');
-                _putchar (' ');
-                }
-                else if (p < 10 && j == 9)
-                {
-                _putchar (p + '0');
-                _putchar (',');
-                _putchar (' ');
+	int i, j, p = 0;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+		p = i * j;
+		if (p < 10 && j != 9)
+		{
+		_putchar (p + '0');
+		_putchar (',');
+		_putchar (' ');
+		}
+		else if (p >= 10 && j != 9)
+		{
+		_putchar ((p / 10) + '0');
+		_putchar ((p % 10) + '0');
+		_putchar (',');
+		_putchar (' ');
+		}
+		else if (p < 10 && j == 9)
+		{
+		_putchar (',');
+		_putchar (' ');
+		_putchar (p + '0');
 		_putchar ('\n');
-                }
-                else if (p >= 10 && j == 9)
-                {
-                _putchar ((p / 10) + '0');
-                _putchar ((p % 10) + '0');
-                _putchar (',');
-                _putchar (' ');
+		}
+		else if (p >= 10 && j == 9)
+		{
+		_putchar (',');
+		_putchar (' ');
+		_putchar ((p / 10) + '0');
+		_putchar ((p % 10) + '0');
 		_putchar ('\n');
-                }
+		}
 
-        }
+	}
 }
 }
