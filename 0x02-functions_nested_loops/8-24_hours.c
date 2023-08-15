660a475cf665378bb4b25d62944848ba9ea8e879
Younes <bdryounesgh@gmail.com>
2023-08-15 15:40:48 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer- prints time
+ *
+ * Return: no return void function.
+ */
+
+void jack_bauer(void)
+{
+	int i = 0, j = 0;
+
+	for (j = 00; j < 24; j++)
+	{
+	for (i = 00; i < 60; i++)
+	{
+		_putchar ('j');
+		_putchar (':');
+		_putchar ('i');
+		_putchar ('\n');
+	}
+	}
+}
