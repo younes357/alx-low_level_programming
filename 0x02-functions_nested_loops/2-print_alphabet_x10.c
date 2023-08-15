902896d162cb8eb74e8bdf93f338ae8d8005a7c7
Younes <bdryounesgh@gmail.com>
2023-08-15 04:03:45 -0700

    alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabetx10-prints the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j = 0;
+
+	for (j = 0; j <= 10; j++)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar (i);
+	}
+	_putchar ('\n');
+	}
+}
