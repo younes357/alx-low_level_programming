09572fb8907388a9bf666cb993f6294c4b9a6531
Younes <bdryounesgh@gmail.com>
2023-08-15 17:29:17 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @m: first input
+ * @n: second input
+ * Return: sum of a and b
+ */
+int add(int m, int n)
+{
+	int p;
+
+	p = n + m;
+i
+	return (c);
+}
