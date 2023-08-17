0b2a3005e3dc57c9a54c5942ec4ecb9ec2722288
Younes <bdryounesgh@gmail.com>
2023-08-17 13:28:45 -0700

    line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,23 +7,23 @@
 
 void print_diagonal(int n)
 {
-	int i, j;
-
 	if (n <= 0)
 	{
-	_putchar('\n');
-	}
-	else
+		_putchar('\n');
+	} else
 	{
-	for (i = 0; i < n; i++)
-	{
-	for (j = 0; j <= i; j++)
-	{
-	_putchar(' ');
-	if (i == j)
-		_putchar('\\');
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
-	}
-}
 }
