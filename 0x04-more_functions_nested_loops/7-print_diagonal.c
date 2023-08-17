b0bf9a609531d9b3d6c05008495beee2435ddeab
Younes <bdryounesgh@gmail.com>
2023-08-17 13:14:48 -0700

    line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonale
+ * @n:  lines's lengh
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+	for (j = 0; j < i ; j++)
+	{
+	_putchar('_');
+	_putchar('\');
+	}
+	_putchar('\n');
+	}
+
+}
