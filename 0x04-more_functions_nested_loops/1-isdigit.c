1f9a5b14b09ea15432c729ad983b58fb921262ba
Younes <bdryounesgh@gmail.com>
2023-08-17 03:46:08 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - determine if the integer c is uppercase
+ * @c: The character to check
+ *
+ * Return: 1 if lowercase or 0 if not.
+ */
+
+int _isdigit(int c)
+{
+if (c < 57 && c > 48)
+	{
+	return (1);
+	}
+else
+	{
+	return (0);
+	}
+}
