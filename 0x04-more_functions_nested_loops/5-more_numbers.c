d832bff3322c99abdfb47201d76b5fd7e4f45dab
Younes <bdryounesgh@gmail.com>
2023-08-17 06:22:48 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * more_numbers - Entry point
+ *
+ */
+
+
+void more_numbers(void)
+	{
+	int c,i;
+	
+	for (i = 0 ; i < 10 ; i++)
+	{	
+	for (c = 0 ; c < 15 ; c++)
+	{
+	_putchar(c + '0');
+	}
+	_putchar('\n');
+	}
+}
