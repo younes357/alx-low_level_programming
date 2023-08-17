a6a30ae29fd9b9aac5fa7a0b17dcbf6a40ae74af
Younes <bdryounesgh@gmail.com>
2023-08-17 05:51:20 -0700

    print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,18 +9,20 @@
 
 void print_most_numbers(void)
 {
+	int c;
+
 	for (c = 48 ; c < 58 ; c++)
 	{
 		if (c == 50 || c == 52)
 		{
 			continue;
 		}
-		else 
+		else
 		{
 			_putchar(c);
 		}
 	}
-	
+
 	_putchar('\n');
 
 }
