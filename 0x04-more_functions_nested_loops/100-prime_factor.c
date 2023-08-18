463d10da56c151333bbbb61579ef63dde8b9a8c3
Younes <bdryounesgh@gmail.com>
2023-08-17 18:15:26 -0700

    functions and loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - entry point
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int n;
+	long int maxPrime = -1;
+	long int i;
+
+	n = 612852475143;
+
+	while (n % 2 == 0)
+	{
+		maxPrime = 2;
+		n >>= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			maxPrime = i;
+			n = n / i;
+		}
+	}
+	/*
+	* This condition is to handle the case
+	* when n is a prime number greater than 4
+	*/
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
+	return (0);
+}
