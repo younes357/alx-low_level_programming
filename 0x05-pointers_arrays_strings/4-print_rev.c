866a57ad1911aa831bc6ee12d919a703a6759d24
Younes <bdryounesgh@gmail.com>
2023-08-21 10:26:27 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - input function.
+ * @str: input
+ *
+ */
+void _puts(char *str)
+{
+int i = _strlen(*str)
+int j;
+
+	for (j = i; j > 0; j--)
+	{
+	
+	_putchar(*(str + j));
+
+}
+_putchar('\n');
+}
