e21ee62f07a7094cd9a16435a20e4ef3eba50131
Younes <bdryounesgh@gmail.com>
2023-08-21 10:54:58 -0700

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,31 +1,29 @@
 #include "main.h"
+
 /**
- * print_rev - input function.
- * @str: input
- *
+ * print_rev - Print a string in reverse order.
+ * @s: The input string.
  */
 void print_rev(char *s)
 {
 int j, i = 0;
 
-while (1)
+// Calculate the length of the string
+while (*s != '\0')
 {
-	if (*s != '\0')
-	{
-		s++;
-		i++;
-	}
-	else
-	{
-		break;
-	}
+s++;
+i++;
 }
 
+// Move back to the last character in the string
+s--;
 
-for (j = 0; j <= i; j--)
-	{
-	
-	_putchar(*(s - 1));
+// Print characters in reverse order
+for (j = 0; j < i; j++)
+{
+_putchar(*(s - j)); // Print characters in reverse order
 }
+
 _putchar('\n');
 }
+
