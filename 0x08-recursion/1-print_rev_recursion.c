859130d880ee5d93eaab35864daa5a2db096d119
younes357 <bdryounesgh@gmail.com>
2023-09-23 08:42:52 -0700

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - input function
+ * @s: pointer to the string
+ *
+ *
+ */
+
+
+
+
+
+void _print_rev_recursion(char *s)
+{
+    if (*s == '\0')
+    {
+    return;
+    }
+
+_print_rev_recursion(s+1);
+
+_putchar(*s);
+}
