936d74e3c5ace9e4e048192c7094ab4f6df11442
younes357 <bdryounesgh@gmail.com>
2023-09-23 08:44:48 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -13,12 +13,12 @@
 
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-    return;
-    }
+	if (*s == '\0')
+	{
+		return;
+	}
 
-_print_rev_recursion(s+1);
+_print_rev_recursion(s + 1);
 
 _putchar(*s);
 }
