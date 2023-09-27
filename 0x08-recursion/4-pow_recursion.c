e21227eda10eb5fd8edcdb91e666e16e9c539b0c
younes357 <bdryounesgh@gmail.com>
2023-09-27 00:31:14 -0700

    recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - input function
+ * @x: input number
+ * @y: power
+ *
+ * Return: number's power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+
+}
