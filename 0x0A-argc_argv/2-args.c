d24aa1eb6f45360401b9fdf0a38e465d950b1128
Younes <bdryounesgh@gmail.com>
2023-11-06 10:52:18 -0800

    argc argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main function
+ * @argc: arguments count
+ * @argv: argument vector
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+int c;
+for (c = 1; c < argc; c++)
+{
+printf("argv[%d] = %s\n", count, argv[count]);
+}
+return (0);
+}
