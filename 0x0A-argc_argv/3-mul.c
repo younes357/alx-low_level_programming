e3dd71ba74cc11c9a7bef5df864b70d89223b46e
Younes <bdryounesgh@gmail.com>
2023-11-18 17:34:28 -0800

    argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-int num1, num2;
+int num1, num2, c;
 if (argc != 3)
 {
 printf("Error\n");
@@ -21,7 +21,8 @@ num2 = atoi(argv[2]);
 
 if (argc == 3)
 {
-printf("%d\n", num1 * num2);
+c = num1 *num2;
+printf("%d\n", c);
 return (0);
 }
 return (0);
