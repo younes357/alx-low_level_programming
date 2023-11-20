d6e15cc03f9130f1c5a3368c052ff7ade8169b47
Younes <bdryounesgh@gmail.com>
2023-11-20 05:56:41 -0800

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - main function
+ * @argc: arguments count
+ * @argv: argument vector
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+
+int c[] = {25, 10, 5, 2, 1};
+int p = atoi(argv[1]), i=0;
+if (argc != 2)
+{       
+        printf("Error\n");
+        return (1);
+} 
+
+if (p < 0)
+{
+	printf("0\n");
+	return (0);
+}
+
+for (i = 0; i < 5 ; i++)
+{
+	while (p >= c[i])
+	{
+		p = p - c[i];
+		n++;
+	}
+}
+printf("%d\n", n);
+return (0);
+}
