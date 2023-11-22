7dc3bff3a4c6a823ffc1f38b1cd348bdc82e48f3
Younes <bdryounesgh@gmail.com>
2023-11-22 10:35:59 -0800

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
+/**
+ * create_array - creates buffer
+ * @c : the address of memory to return
+ * @size: the size of the memory to print
+ *
+ * Return: Returns a pointer to the array, or NULL if it fails
+ */
+
 char *create_array(unsigned int size, char c)
 {
 char *v;
 unsigned int s;
-v = malloc (size * sizeof (char));
+if (size == 0)
+{
+return (NULL);
+}
+v = malloc(size * sizeof(char));
+if (v == NULL)
+{
+return (NULL);
+}
 for (s = 0; s < size; s++)
 {
 v[s] = c;
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+
+
+#endif
