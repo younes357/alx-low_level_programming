0efaaf357dd75c9bab691afbbde10efacdfeed9c
Younes <bdryounesgh@gmail.com>
2023-11-22 10:52:08 -0800

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * _strdup - creates reprint input array
+ * @str : the address of memory to return
+ *
+ * Return: Returns a pointer to the array, or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+char *s, *p;
+s = malloc(sizeof(char));
+s = str;
+p = s;
+return (p);
+}
